cbd05ce11480c7db85d7494d6377d9827640aee2
Floramihran <162467102+Floramihran@users.noreply.github.com>
2025-09-14 19:29:18 +0400

    Update assignment0.c

--- HW2/assignment0.c
+++ HW2/assignment0.c
@@ -4,20 +4,23 @@
 
 int main(){
 	int ret1 = fork();
-	if(ret1 == 0){
-	printf("After 1st fork, Child: PID=%d , PPID=%d\n", getpid(), getppid());
+    if(ret1 == 0){
+	    printf("After 1st fork, Child: PID=%d , PPID=%d\n", getpid(), getppid());
 	}else{
-	 printf("After 1st fork, Parent: PID=%d , ChildPID=%d\n", getpid(), ret1);
-	
+	    printf("After 1st fork, Parent: PID=%d , ChildPID=%d\n", getpid(), ret1);
 	}
 
 	int ret2 = fork();
-        if(ret2 == 0){                                                              printf("After 2nd fork, Child: PID=%d , PPID=%d\n", getpid(), getppid());                                                                                }else{
+     if(ret2 == 0){       
+		printf("After 2nd fork, Child: PID=%d , PPID=%d\n", getpid(), getppid()); 
+	}else{
         printf("After 2nd fork, Parent: PID=%d , ChildPID=%d\n", getpid(), ret2);
         }
 	
 	int ret3 = fork();
-        if(ret3 == 0){                                                              printf("After 3rd fork, Child: PID=%d , PPID=%d\n", getpid(), getppid());                                                                                }else{
+    if(ret3 == 0){              
+		printf("After 3rd fork, Child: PID=%d , PPID=%d\n", getpid(), getppid());   
+	}else{
          printf("After 3rd fork, Parent: PID=%d , ChildPID=%d\n", getpid(), ret3);
         }
 	
