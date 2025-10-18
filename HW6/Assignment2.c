72ec76252aca1f24086a1e61f4bb086a4e502e2a
Floramihran <162467102+Floramihran@users.noreply.github.com>
2025-10-18 19:39:05 +0400

    Update Assignment2.c

--- HW6/Assignment2.c
+++ HW6/Assignment2.c
@@ -36,21 +36,21 @@ int main(){
 	struct Point2 p2;
 
 	printf("Size of struct Point2: %lu bytes.\n", sizeof(struct Point2));
-        printf("Address of c: %p\n", (void*)&p2.c);
-        printf("Address of b: %p\n", (void*)&p2.b);
-        printf("Address of a: %p\n", (void*)&p2.a);
+    printf("Address of c: %p\n", (void*)&p2.c);
+    printf("Address of b: %p\n", (void*)&p2.b);
+    printf("Address of a: %p\n", (void*)&p2.a);
 
-        printf("\n");
+    printf("\n");
 
 
 	struct Point3 p3;
 
 	printf("Size of struct Point3: %lu bytes.\n", sizeof(struct Point3));
-        printf("Address of b: %p\n", (void*)&p3.b);
-        printf("Address of a: %p\n", (void*)&p3.a);
-        printf("Address of c: %p\n", (void*)&p3.c);
+    printf("Address of b: %p\n", (void*)&p3.b);
+    printf("Address of a: %p\n", (void*)&p3.a);
+    printf("Address of c: %p\n", (void*)&p3.c);
 
-        printf("\n");
+    printf("\n");
 
 
 	return 0;
