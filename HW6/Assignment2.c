9192a1edfc24a5f14a364c0bb44502989c6ca678
Floramihran <162467102+Floramihran@users.noreply.github.com>
2025-10-18 19:40:54 +0400

    Update Assignment2.c

--- HW6/Assignment2.c
+++ HW6/Assignment2.c
@@ -26,31 +26,31 @@ int main(){
 	
 	struct Point1 p1;
 	
-	printf("Size of struct Point1: %lu bytes.\n", sizeof(struct Point1));
-	printf("Address of a: %p\n", (void*)&p1.a);
-	printf("Address of b: %p\n", (void*)&p1.b);
-	printf("Address of c: %p\n", (void*)&p1.c);
+		printf("Size of struct Point1: %lu bytes.\n", sizeof(struct Point1));
+		printf("Address of a: %p\n", (void*)&p1.a);
+		printf("Address of b: %p\n", (void*)&p1.b);
+		printf("Address of c: %p\n", (void*)&p1.c);
 
-	printf("\n");
+		printf("\n");
 
 	struct Point2 p2;
 
-	printf("Size of struct Point2: %lu bytes.\n", sizeof(struct Point2));
-    printf("Address of c: %p\n", (void*)&p2.c);
-    printf("Address of b: %p\n", (void*)&p2.b);
-    printf("Address of a: %p\n", (void*)&p2.a);
+		printf("Size of struct Point2: %lu bytes.\n", sizeof(struct Point2));
+    	printf("Address of c: %p\n", (void*)&p2.c);
+    	printf("Address of b: %p\n", (void*)&p2.b);
+    	printf("Address of a: %p\n", (void*)&p2.a);
 
-    printf("\n");
+    	printf("\n");
 
 
 	struct Point3 p3;
 
-	printf("Size of struct Point3: %lu bytes.\n", sizeof(struct Point3));
-    printf("Address of b: %p\n", (void*)&p3.b);
-    printf("Address of a: %p\n", (void*)&p3.a);
-    printf("Address of c: %p\n", (void*)&p3.c);
+		printf("Size of struct Point3: %lu bytes.\n", sizeof(struct Point3));
+    	printf("Address of b: %p\n", (void*)&p3.b);
+   		printf("Address of a: %p\n", (void*)&p3.a);
+    	printf("Address of c: %p\n", (void*)&p3.c);
 
-    printf("\n");
+    	printf("\n");
 
 
 	return 0;
