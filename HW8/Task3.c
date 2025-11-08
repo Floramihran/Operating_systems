dbf00be4a98dbe5deb754d3dc07c4fd67f708564
Floramihran <ambardjianflora@gmail.com>
2025-11-08 19:46:56 +0000

    added new file

--- HW8/Task3.c
+++ HW8/Task3.c
@@ -12,42 +12,46 @@ int main(){
 	int bytes_read;
 	char buffer[1];
 
-	write(STDOUT_FILENO, "Enter file path:   ", 19);
-	bytes_read = read(STDIN_FILENO, file_path, sizeof(file_path) -1);
-	if (bytes_read <= 0){
-		perror("read file path");
-		return 1; 
-	}
-	file_path[bytes_read -1] = '\0';
+	write(STDOUT_FILENO, "Enter file path: ", 17);
+        bytes_read = read(STDIN_FILENO, file_path, sizeof(file_path) - 1);
+        if (bytes_read <= 0)
+        {
+       		perror("read file path");
+        	return 1;
+    	}
+    	file_path[bytes_read - 1] = '\0';
 
-	fd = open(file_path, O_RDONLY);
-	if (fd == -1){
-		perror("open failed");
-		return 1;
-	}
+    	fd = open(file_path, O_RDONLY);
+    	if (fd == -1)
+    	{
+        	perror("open't");
+        
+        	return 2;
+    	}
 
-	file_size = lseek(fd, 0, SEEK_END);
-	if(file_size == -1){
-		perror("lseek failed");
-		close(fd);
-		return 1;
-	}
+    	file_size = lseek(fd, 0, SEEK_END);
+    	if (file_size == -1)
+    	{
+        	perror("lseek");
+        	close(fd);
+        	return 3;
+    	}
 
-	for (int i = file_size -1; i >= 0; i--){
-		if (lseek(fd, i, SEEK_SET) == -1){
-		
-			perror("lseek failed");
-			break;
-		}
-		if (read(fd, buffer, 1) == 1){
-			write(STDOUT_FILENO, buffer, 1);
-		
-		}
-		write(STDOUT_FILENO, "\n", 1);
+   	for (int pos = file_size - 1; pos >= 0; pos--)
+    	{
+        	if (lseek(fd, pos, SEEK_SET) == -1){
+        	    perror("lseek");
+           	    break;
+        	}
+        	if (read(fd, buffer, 1) == 1){
+            		write(STDOUT_FILENO, buffer, 1);
+       		}
+    	}
+    	
+	write(STDOUT_FILENO, "\n", 1);
 
-		close(fd);
+    	close(fd);
 
-		return 0;
-	
-	}
+    	return 0;
 }
+
